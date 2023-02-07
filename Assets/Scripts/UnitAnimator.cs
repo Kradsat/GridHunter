27051dc541c8b8c262e71eab8fe6883cfbf4339c
AndresAdriano <65266823+Kradsat@users.noreply.github.com>
2023-02-07 14:46:22 +0900

    animation_update
    didnt want to merge the branch so manually change the stuff XDDD

--- Assets/Scripts/UnitAnimator.cs
+++ Assets/Scripts/UnitAnimator.cs
@@ -26,7 +26,7 @@ public class UnitAnimator : MonoBehaviour
         if (TryGetComponent<AttackAction>(out AttackAction attackAction))
         {
             attackAction.OnAttackStart += AttackAction_OnAttackStart;
-            //attackAction.OnAttackEnd += AttackAction_OnAttackEnd;
+            attackAction.OnAttackEnd += AttackAction_OnAttackEnd;
         }
 
         if (TryGetComponent<SpecialAttack>(out SpecialAttack specialAttack))
@@ -41,22 +41,51 @@ public class UnitAnimator : MonoBehaviour
     private void SpecialAttack_OnAttackStart(object sender, EventArgs e)
     {
         animator.SetTrigger("SpecialAttack");
+         var childComponents = GetComponentsInChildren<Animator>();
+        foreach (var child in childComponents)
+        {
+           child.SetBool("Attack1",true);
+        }
     }
 
     private void AttackAction_OnAttackStart(object sender, EventArgs e)
     {
-        animator.SetTrigger("Attack");
+        animator.SetBool("Attack1",true);
+        var childComponents = GetComponentsInChildren<Animator>();
+        foreach (var child in childComponents)
+        {
+           child.SetBool("Attack1",true);
+        }
     }
 
+     private void AttackAction_OnAttackEnd(object sender, EventArgs e)
+    {
+        animator.SetBool("IsWalking", false);
+        var childComponents = GetComponentsInChildren<Animator>();
+        foreach (var child in childComponents)
+        {
+           child.SetBool("Attack1", false);
+        }
+    }
 
     private void MoveAction_OnStartMoving(object sender, EventArgs e)
     {
         animator.SetBool("IsWalking", true);
+        var childComponents = GetComponentsInChildren<Animator>();
+        foreach (var child in childComponents)
+        {
+           child.SetBool("IsWalking", true);
+        }
     }
 
     private void MoveAction_OnStopMoving(object sender, EventArgs e)
     {
         animator.SetBool("IsWalking", false);
+        var childComponents = GetComponentsInChildren<Animator>();
+        foreach (var child in childComponents)
+        {
+           child.SetBool("IsWalking", false);
+        }
     }
 
 
